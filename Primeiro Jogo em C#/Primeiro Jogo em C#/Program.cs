edf1cb90c385d1b0b6aea2aa2ecf5c5e7efa7389
Sasuzin <lfelipepassos1@gmail.com>
2023-08-10 22:03:05 -0300

    Ajustes visuais nos projetos

--- Primeiro Jogo em C#/Primeiro Jogo em C#/Program.cs
+++ Primeiro Jogo em C#/Primeiro Jogo em C#/Program.cs
@@ -3,7 +3,7 @@ int numeroSecreto = aleatorio.Next(1, 101);
 
 do
 {
-    Console.Write("Digite um número entre 1 e 100: ");
+    Console.Write("\nDigite um número entre 1 e 100: ");
     int chute = int.Parse(Console.ReadLine());
 
     if (chute == numeroSecreto)
@@ -13,11 +13,11 @@ do
     }
     else if (chute < numeroSecreto)
     {
-        Console.WriteLine("O número é maior.");
+        Console.WriteLine("\nO número é maior.");
     }
     else
     {
-        Console.WriteLine("O número é menor.");
+        Console.WriteLine("\nO número é menor.");
     }
 
 } while (true);
